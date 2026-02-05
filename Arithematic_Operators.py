d2be2a0e3993b862bb2b2c9b3fe50e25fecad555
22bec002-iiitDwd <22bec002@iiitdwd.ac.in>
2026-02-05 19:33:33 +0530

    Day 1: Arithematic Operators for Mathematical operations

---
+++ Arithematic_Operators.py
@@ -0,0 +1,9 @@
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(12//5)      # (//) floor operator always shifts the number to the 'left' and gives the 'integer value to the left of the number line' -> Example : 2.1 = 2, -2.6 = -3
+print(-12//5)     # (-2.4 = -3) 
+# print(5 // 0)   # raises zero division error
+print(5%3)        # (%)  modulo operator for remainder: 5%3 = 2
+print(5**2)       # (**) it is an exponential operator for powers
