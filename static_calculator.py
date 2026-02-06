46798d81fd85dc5a6f4365554a5f69081763f852
22bec002-iiitDwd <22bec002@iiitdwd.ac.in>
2026-02-06 17:33:04 +0530

    Day 2: Static Calculator for simple calculations

---
+++ static_calculator.py
@@ -0,0 +1,20 @@
+# aasigning the values to "a" and "b"
+a = 5  
+b = 6
+
+print("\n")
+print( " the two numbers are : " , a , " and " , b )                 # printing the two numbers and taking operations in the next line 
+ 
+print( "sum of two numbers are : " , a + b )                         # printing the sum of the numbers
+
+print( "difference of two numbers are : " , a - b )                  # printing the difference of the numbers
+
+print( " multiplication of two numbers are : " , a * b )             # printing the multiplication of numbers
+
+print( " division of two numbers are : " , a / b )                   # printing the division of the numbers
+
+print(" floor division of two number are : " , a // b)               # printing the floor division of two numbers
+
+# some shortcuts of VScode
+# (alt + tap) provides edit to multiple positions at the same time
+# (alt + shift + down)  replicate the selected line of code
