1d9a427e2dfce02799e120546cd79d793cfb5a2b
22bec002-iiitDwd <22bec002@iiitdwd.ac.in>
2026-02-07 18:22:35 +0530

    Day 3: Adding element inthe list at the end

---
+++ append_list.py
@@ -0,0 +1,8 @@
+nums= [ 1, 2, 3, [4,5] ]  # list with [] square brackets and contained ordered data with 'another list' -> [4,5]
+
+nums.append(11)      # adding the integer 11 at the end of the list
+nums.append(27)    
+nums.append(2002)  
+nums.append(2006)  
+   
+print(nums)
