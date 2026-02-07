8cb83593f230eef097b662af875019ca081096b8
22bec002-iiitDwd <22bec002@iiitdwd.ac.in>
2026-02-07 18:25:42 +0530

    Day 3: Taking user input

---
+++ user_input.py
@@ -0,0 +1,18 @@
+# user input is taken as string only but we can convert them to different data types using "typecasting"
+a = input("enter your name : ")    # input() function takes the input only as string data type
+
+print(" my name is : " , a)
+
+######################################
+
+x = int(input("enter your first number : "))    # converting the string input in integer type first 
+y = int(input("enter your second number : "))   # converting the string input in integer type first
+print("Your answer is : " , x + y )
+
+# Note : but enter only mentioned type during inputs at output window
+
+
+
+
+
+
